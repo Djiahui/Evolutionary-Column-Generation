f998593fc359cf6013db522249f1f32685775335
Djiahui <2019223045211@stu.scu.edu.cn>
2021-04-25 23:03:32 +0800

    EA-assisted

---
+++ EA_assisted.py
@@ -0,0 +1,40 @@
+import numpy as np
+class Population(object):
+	def __init__(self,size,customer_number,eva):
+		self.size = size
+		self.customer_number = customer_number
+		self.population = []
+		self.eva = eva
+	def pop_generate(self):
+		# random_generate
+		for i in range(self.size):
+			temp = Individual(self.customer_number)
+			temp.cost = self.eva.evaluate(temp.x)
+
+	def update(self):
+		pass
+
+
+
+class Individual(object):
+	def __init__(self,variable_size):
+		self.variable_size = variable_size
+		self.x = np.random.rand(variable_size)
+		self.cost = None
+
+
+
+
+class Evaluator(object):
+	def __init__(self,dis,customers,capacity,customer_number):
+		self.pi = None
+		self.dis = dis
+		self.customers = customers
+		self.customer_number = customer_number
+		self.capacity = capacity
+	def evaluate(self,x):
+		# greedy_decoder
+		index = x.argsort()
+
+
+
